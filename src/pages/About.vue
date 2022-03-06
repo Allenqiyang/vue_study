5d73caf1920d0d2a4790241153ca043dff7ba4de
Allenqiyang <89894318+Allenqiyang@users.noreply.github.com>
2022-03-06 14:31:07 +0800

    Add files via upload

---
+++ src/pages/About.vue
@@ -0,0 +1,25 @@
+<template>
+    <h2>我是About的内容</h2>
+</template>
+
+<script>
+export default {
+    name:"About",
+    //通过路由规则进入该组件时调用
+    beforeRouteEnter (to, from, next) {
+        if(to.meta.isAuth){     //判断是否需要鉴权
+            if(localStorage.getItem('school')==='LuoWai'){
+                next()
+            }else{
+                alert('School is not right! No authorized')
+            }
+        }else{
+            next()
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
