843fa42dfe397fbe171d58e5621058833360814b
Allenqiyang <89894318+Allenqiyang@users.noreply.github.com>
2022-04-08 13:31:15 +0800

    Add files via upload

---
+++ vue3_study/18_src_suspense/App.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="app">
+  <h3>我是祖先组件</h3>
+  <Suspense>
+    <template v-slot:default>
+      <Child></Child>
+    </template>
+    <template v-slot:fallback>
+      <h4>loading... Please wait...</h4>
+    </template>
+  </Suspense>
+</div>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+// import Child from './components/Child.vue'    //静态引入
+const Child = defineAsyncComponent(()=>import('./components/Child.vue'))    //异步引入
+export default {
+  name: 'App',
+  components:{Child},
+}
+</script>
+
+<style>
+.app{
+  background-color:grey;
+  padding: 10px;
+}
+</style>
---
+++ vue3_study/18_src_suspense/components/Child.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="child">
+        <h3>我是Child组件</h3>
+        {{sum}}
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    name: 'Child',
+    setup(){
+        const sum = ref(0)
+        // 使用了suspense，使用了异步引入就可以返回Promise对象
+        return new Promise((resolve, reject) =>{
+            setTimeout(()=>{
+                resolve(sum)
+            },2000)
+        })
+    }
+}
+</script>
+
+<style>
+.child{
+    background-color: skyblue;
+    padding: 10px;
+}
+</style>
