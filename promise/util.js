99f0aa9c51b44187be67a91f44b2d3f235e0bade
Allenqiyang <89894318+Allenqiyang@users.noreply.github.com>
2022-04-12 16:31:11 +0800

    Delete promise directory

--- promise/util.js
+++
@@ -1,7 +0,0 @@
-const util = require('util');
-const fs = require('fs');
-let myReadFile = util.promisify(fs.readFile);
-
-myReadFile('__dirname/../text/study.md').then(value => {
-    console.log(value.toString());
-})
